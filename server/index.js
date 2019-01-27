e51cb24672e031c5ea421fa6eae5cd56c9687d30
Ammar <ammar.alnatsheh@gmail.com>
2019-01-26 23:08:51 -0800

    test with sample data

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const { data } = require('../database/sampleData');
 
 const app = express();
 const PORT = 8080;
@@ -8,9 +9,10 @@ const PORT = 8080;
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
 
-app.get('/events/:catagory', (req, res) => {
-  const cat = req.params.catagory;
-  res.status(200).end(`get events ${cat}`);
+app.get('/events', (req, res) => {
+  // const cat = req.params.catagory;
+  console.log(data);
+  res.status(200).end(JSON.stringify(data));
 });
 app.post('/event', (req, res) => {
   res.status(201).end('post event');
