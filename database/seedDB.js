60fc5124ba14a2e2e0a9404f4857c3fc156470c5
Ammar <ammar.alnatsheh@gmail.com>
2019-01-27 20:01:00 -0800

    add hints

--- database/seedDB.js
+++ database/seedDB.js
@@ -3,3 +3,14 @@ const generateEvents = require('./generateEvents');
 generateEvents(() => {
   process.exit(1);
 });
+
+// mongoimport -d events -c Events --type tsv --file 'eventsData.tsv' -f
+// _id,name,title,catagory,date,time,description  --numInsertionWorkers 8
+
+// update the id counter after seeding
+//   const eventIdCounter = {
+//     count: 1000,
+//     model: 'Events',
+//     field: '_id',
+//     __v: 0,
+//   }
