811d1ea7547c68b1be917e7cbf3fa9b164751e86
Ammar <ammar.alnatsheh@gmail.com>
2019-01-27 20:07:23 -0800

    build the server routs

--- server/index.js
+++ server/index.js
@@ -1,25 +1,45 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const { data } = require('../database/sampleData');
+const { mongoDB } = require('../database/mongo_modules');
 
 const app = express();
 const PORT = 8080;
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
 
 app.get('/events', (req, res) => {
-  // const cat = req.params.catagory;
-  console.log(data);
-  res.status(200).end(JSON.stringify(data));
+  const filter = req.body;
+  mongoDB.selectEvents(filter, (err, data) => {
+    if (err) {
+      res.status(501).send(err);
+    } else {
+      res.status(200).end(data);
+    }
+  });
 });
+
 app.post('/event', (req, res) => {
-  res.status(201).end('post event');
+  const event = req.body;
+  mongoDB.insertEvent(event, (err, data) => {
+    if (err) {
+      res.status(501).send(err);
+    } else {
+      res.status(200).end(data);
+    }
+  });
 });
+
 app.delete('/event/:eventId', (req, res) => {
-  const id = req.params.eventId;
-  res.status(201).end(`delete event ${id}`);
+  mongoDB.deleteEvent(req.params.eventId, (err, data) => {
+    if (err) {
+      res.status(501).send(err);
+    } else {
+      res.status(203).send(data);
+    }
+  });
 });
 
 app.listen(PORT, () => {
